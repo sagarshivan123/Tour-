db0af304cb08c3b8ac12c2dd66988716ad3df93d
Shivan Sagar Gupta <guptajisagar414@gmail.com>
2026-02-25 22:24:08 +0530

    Fixed alt attribute error in Card component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -14,7 +14,7 @@ function readmoreHandler(){
 
 
 <div className="card">
-<img src={image} className="image"></img>
+<img src={image} alt={image.name} className="image"></img>
 
 <div className="tour-info">
 <div className="tour-details">
